59c5895d5cf32a9f89db6659b4b609c31c5c74c9
LukasFaller <fallerlukas@web.de>
2016-12-07 13:22:56 +0100

    Added classes Direction and Settlement Updated class Field

--- src/main/java/de/simichki/overworld/Field.java
+++ src/main/java/de/simichki/overworld/Field.java
@@ -8,4 +8,15 @@ public class Field {
     private int yPos;
     private FieldType fieldType;
     private Structure structure;
+
+    public Field(int xPos, int yPos, FieldType fieldType) {
+        this.xPos = xPos;
+        this.yPos = yPos;
+        this.fieldType = fieldType;
+    }
+
+    public Field getNeighbour(Direction direction) {
+
+    }
+
 }
